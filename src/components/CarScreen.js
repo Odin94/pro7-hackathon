fd479d1aa955ba99c85f3224e04b69c022a9cee7
Andreas Kammerloher <kammerloher.andreas@gmail.com>
2018-10-20 20:47:36 +0200

    Fix class -> className

--- src/components/CarScreen.js
+++ src/components/CarScreen.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Header from './Header';
+import EntertainmentInfo from './EntertainmentInfo';
 import Eta from './Eta';
 import Location from './Location';
 
 import fullMap from '../assets/map_full.png'
 
-const CarScreen = function(props) {
+const CarScreen = function({update_live_events}) {
     return (
-        <div class="container">
+        <div className="container">
             <Header destinationName="ibis Hotel Garching" />
             <Location image={fullMap} />
             <Eta time="20:00"/>
+            <EntertainmentInfo update_live_events={update_live_events}/>
         </div>
     );
 };

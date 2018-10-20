65829ca23d78da1a8b1ba25f2974e7912bbcc995
Andreas Kammerloher <kammerloher.andreas@gmail.com>
2018-10-20 16:33:04 +0200

    Implement ETA component

--- src/components/Eta.js
+++ src/components/Eta.js
@@ -1,14 +1,29 @@
 import React from 'react';
 
-const ETA = (props) => {
+const ETA = ({time}) => {
 
 
     return (
-        <div>
-
-        </div>
+        <section className="row" style={styles.eta}>
+            <div className="col">
+                <i className="far fa-clock" style={styles.eta.clockIcon}></i>
+                ETA:
+            </div>
+            <div className="col" style={styles.eta.time}>{time}</div>
+        </section>
     )
 }
 
+const styles = {
+    eta: {
+        fontSize: '3em',
+        clockIcon: {
+            marginRight: '15px'
+        },
+        time: {
+            textAlign: 'center'
+        }
+    }
+};
 
 export default ETA;

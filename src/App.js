66360e20da2098375e5be6cab94fd16f81b73d8d
Maximilian Stadlmeier <info@mstadlmeier.de>
2018-10-20 16:41:29 +0200

    Airport map

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Textfield from './components/Textfield';
+import Location from './components/Location';
 //NDguMzUxLDExLjc3OSw0OC4yNDcsMTEuNjMwLGliaXMgSG90ZWwgR2FyY2hpbmcsTWVyY2VkZXMgQmVueiBBTUcgQ0xBLEtvbmZlcmVuejogTW9udGFnIDEyOjAwIGltIEhvdGVs
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
           <Header destinationName="ibis Hotel Garching" />
         </header>
         <Textfield headline="Test" paragraph="Your car is waiting for you"/>
+        <Location />
       </div>
     );
   }
--- src/components/Location.js
+++ src/components/Location.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import mapAirport from '../assets/map_airport.png'
 
 const Location = (props) => {
 
 
     return (
         <div>
-
+            <img src={mapAirport} />
         </div>
     )
 }

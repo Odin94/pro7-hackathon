7239c5eba65f03f10c6537ac0a24fa410d11937e
Maximilian Stadlmeier <info@mstadlmeier.de>
2018-10-21 11:10:33 +0200

    Turn car into card

--- src/components/CarInfo.js
+++ src/components/CarInfo.js
@@ -7,13 +7,13 @@ class CarInfo extends Component {
 
     render() {
         return (
-            <div>
-                <div className="row" style={{marginTop: '10px'}}>
+            <div style={{boxShadow: '2px 2px 2px 2px rgba(0,0,0,0.2)', padding: '5px', marginTop: '10px', marginBottom: '20px'}}>
+                <div className="row">
                     <div className="col-3">
                         <img style={{borderRadius: '4px', borderWidth: '2px', borderColor: '#000'}} src={this.props.carImg}></img>
                     </div>
                     <div className="col-9 align-self-center">
-                        <span style={{marginLeft: '30px', fontSize: '18px'}}>{this.props.carName}</span>
+                        <span style={{marginLeft: '35px', fontSize: '18px'}}>{this.props.carName}</span>
                     </div>
                 </div>
             </div>
--- src/components/Location.js
+++ src/components/Location.js
@@ -4,7 +4,7 @@ const Location = ({enterCar, image}) => {
 
     return (
         <a onClick={ () => {if (enterCar) enterCar()} }>
-            <img style={{boxShadow: '2px 2px rgba(0,0,0,0.2)', width: '100%'}} src={image} alt="map" />
+            <img style={{boxShadow: '2px 2px 2px 2px rgba(0,0,0,0.2)', width: '100%'}} src={image} alt="map" />
         </a>
     )
 }
